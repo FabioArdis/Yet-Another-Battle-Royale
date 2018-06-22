3967be0d0c5d1e7882589f66404662f1dd42cb0e
Fabio <FabioArdis@users.noreply.github.com>
2018-06-22 18:34:57 +0200

    The update everyone wanted
    (not really)

--- main.cpp
+++ main.cpp
@@ -1,201 +1,114 @@
-// ConsoleApplication32.cpp: definisce il punto di ingresso dell'applicazione console.
-//
-
-#include "stdafx.h" // VS
 #include <iostream>
 #include <string.h>
 #include <time.h>
 
-#define NUMSURVIVOR 4 // Player
-#define MAXKFR 3 //  Max kill for round
-#define NOMELEN 12 // Nome è un array e serve una costante
-#define VINCITORE 1 // Magari vincono in 2(?)
-#define NOKILL 0 // Ovviamente
-#define INITROUND 1 // Idem
+using namespace std;
 
-typedef struct
+int players = 12;
+int playersAlive = players;
+int playersDead = 0;
+int round = 1;
+int countPosition = players;
+
+string names[60] = {
+	"Sam", "Jude", "Leonard", "Alicia",
+	"Max", "Martha", "Alexander", "Rose",
+	"Dexter", "Lauren", "Cody", "Shannon",
+	"Alex", "Lara", "Jordan", "Rebecca",
+	"Sean", "Bella", "Scott", "Molly",
+	"Noah", "Ellie", "Zac", "Sienna",
+	"Matthew", "Marianna", "Jake", "Linda",
+	"Lee", "Ruby", "Kane", "Riley",
+	"Nathan", "Monica", "Bobby", "Zoe",
+	"Tom", "Carla", "Fabio", "Emilia",
+	"Roger", "Faith", "Caleb", "Rosy",
+	"Logan", "Isabella", "Josh", "Taylor",
+	"Kevin", "Jasmine", "Daniel", "Lorelai",
+	"Charlie", "Lydia", "Jonah", "Scarlett",
+	"Paul", "Lucy", "Robert", "Autumn",
+};
+
+struct Player
 {
-	char nome[NOMELEN];
-	int id;
-	bool vivo=true;
-}SURVIVOR;
+	string name;
+	bool isAlive = true;
+	int position = players;
+};
 
-void gioca();
-void inizializza_tutti(SURVIVOR giocatori[]);
-void inizializza_id(SURVIVOR *player, int index);
-void nomina_tutti(SURVIVOR giocatori[]);
-void nomina(SURVIVOR *player);
-void muori(SURVIVOR *player);
-void roba_random(SURVIVOR giocatori[], int alive, int *index);
-void notifica_uccisione(SURVIVOR giocatori[], int index);
-void reset_id(SURVIVOR giocatori[], int numalive);
-void mortiid(SURVIVOR giocatori[]);
-void havinto(SURVIVOR giocatore);
-void vittoria(SURVIVOR giocatori[]);
-int contavivi(SURVIVOR giocatori[]);
-int contamorti(SURVIVOR giocatori[]);
+struct Player* Players = new Player[players];
 
-int main()
+void resetPlayers();
+void start();
+void playRound();
+void playerEvent();
+
+int main(int argc, char const *argv[])
 {
-	srand(time(NULL));
-	gioca();
-	std::cin.get();
-	std::cin.get();
-    return 0;
+	start();
+	return 0;
 }
 
-void gioca()
+void playRound()
 {
-	int index;
-	int alive;
-	int round=INITROUND;
-	int kts;
-	SURVIVOR giocatori[NUMSURVIVOR];
-	inizializza_tutti(giocatori);
-	nomina_tutti(giocatori);
-	do
-	{
-		printf("\n");
-		alive = contavivi(giocatori);
-		if (alive != VINCITORE)
-		{
-			printf("Round %d:\n\n", round);
-			kts = rand() % MAXKFR; //Kill this round
-			if(kts!=NOKILL)
-			{
-				for(int j=0;j<kts;j++)
-				{
-				roba_random(giocatori, alive, &index);
-				notifica_uccisione(giocatori, index);
+	cout << "Round " << round << endl;
+	cout << "Players alive: " << playersAlive << endl << endl;
+	playerEvent();
+	round++;
+	cout << endl << endl;
+	playRound();
+
+}
+
+void playerEvent()
+{
+	if(playersAlive > 1){
+		for (int i = 0; i < players; ++i)
+			if(Players[i].isAlive == true && playersAlive > 1)
+				if((rand()%2) == 1){
+					playersAlive--;
+					cout << Players[i].name << " dies." << endl;
+					Players[i].isAlive = false;
+					Players[i].position = countPosition--;
+					cout << Players[i].name << " classified " << Players[i].position << endl;
 				}
+	}
+	else
+	{
+		for (int i = 0; i < players; ++i)
+			if(Players[i].isAlive == true){
+				int selection = 1;
+				cout << Players[i].name << " wins!" << endl << endl;
+				cout << "Do you want to retry?1-Yes 0-No" << endl;
+				cin >> selection;
+				if(selection == 1)
+					start();
+				else
+					exit(EXIT_SUCCESS);
 			}
-			else
-			{
-				printf("Nessuno è morto\n");
-			}
-		}
-		else // Boh
-		{
-			vittoria(giocatori);
-		}
-		round++;
-	} while (alive > VINCITORE);
-	vittoria(giocatori); // Boh 2
-}
-
-void inizializza_tutti(SURVIVOR giocatori[])
-{
-	for (int j = 0; j < NUMSURVIVOR; j++)
-	{
-		inizializza_id(&giocatori[j], j);
 	}
 }
 
-void inizializza_id(SURVIVOR *player, int index)
+void start()
 {
-	player->id = index;
+	cout << "Select Players." << endl;
+	cin >> players;
+	resetPlayers();
+	playRound();
 }
 
-void nomina_tutti(SURVIVOR giocatori[])
+void resetPlayers()
 {
-	for (int j = 0; j < NUMSURVIVOR; j++)
+	delete [] Players;
+	struct Player* Players = new Player[players];
+	for (int i = 0; i < players; ++i)
 	{
-		nomina(&giocatori[j]);
+		int r = (rand()%60);
+		Players[i].name = names[r];
+		Players[i].isAlive = true;
+		Players[i].position = players;
 	}
-}
-
-void nomina(SURVIVOR *player)
-{
-	printf("Inserisci nome player %d\n", player->id + 1);
-	//scanf("%s", player->nome);
-	scanf_s("%s", player->nome, NOMELEN+1);
-}
-
-void muori(SURVIVOR *player)
-{
-	player->vivo = false;
-}
-
-void roba_random(SURVIVOR giocatori[], int alive, int *index)
-{
-		reset_id(giocatori, alive);
-		do
-		{
-			*index = rand() % alive;
-		} while (giocatori[*index].vivo != true);
-		muori(&giocatori[*index]);
-}
-
-void notifica_uccisione(SURVIVOR giocatori[], int index)
-{
-	printf("%s è morto\n", giocatori[index].nome);
-}
-
-int contavivi(SURVIVOR giocatori[])
-{
-	int vivi=0;
-	for (int j = 0; j < NUMSURVIVOR; j++)
-	{
-		if (giocatori[j].vivo != false)
-		{
-			vivi++;
-		}
-	}
-	return vivi;
-}
-
-int contamorti(SURVIVOR giocatori[])
-{
-	int morti = 0;
-	for (int j = 0; j < NUMSURVIVOR; j++)
-	{
-		if (giocatori[j].vivo != true)
-		{
-			morti++;
-		}
-	}
-	return morti;
-}
-
-void reset_id(SURVIVOR giocatori[], int numalive) // Qua si sfancula tutto
-{
-	int morti=0;
-	for (int j = 0; j < numalive; j++)
-	{
-		if (giocatori[j].vivo == true)
-		{
-			inizializza_id(&giocatori[j], (morti-j));
-		}
-		else
-		{
-			morti++;
-		}
-	}
-}
-
-void mortiid(SURVIVOR giocatori[]) // Idem sopra
-{
-	for (int j = 0; j < NUMSURVIVOR; j++)
-	{
-		if (giocatori[j].vivo != true)
-		{
-			giocatori[j].id = NUMSURVIVOR + giocatori[j].id;
-		}
-	}
-}
-
-void havinto(SURVIVOR giocatore)
-{
-	printf("Ha vinto %s!\n", giocatore.nome);
-}
-
-void vittoria(SURVIVOR giocatori[])
-{
-	for (int j = 0; j < VINCITORE; j++)
-	{
-		if (giocatori[j].vivo != false)
-		{
-			havinto(giocatori[j]);
-		}
-	}
+	playersAlive = players;
+	playersDead = 0;
+	round = 1;
+	countPosition = players;
 }
